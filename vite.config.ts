8549c7d3f77cebdfa3c4119969b871ed8bb54fa0
Sumin <robolindasoo@gmail.com>
2024-01-06 16:30:37 +0900

    chore: msw 파일 추가

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,11 @@ export default defineConfig({
     },
   },
   server: {
+    port: 3000,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
     proxy: {
       '/api': {
         target: 'https://dev.gomterview.com',
