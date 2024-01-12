1754c0f5012c0d0cf736054c7fd6a148a5705749
adultlee <seoungin1228@gmail.com>
2024-01-12 16:50:31 +0900

    hotfix:  baseURL 변경

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,7 +1,7 @@
 type Id = number;
 type Hash = string;
 
-export const BASE_URL = process.env.REACT_APP_BASE_API_URL;
+export const BASE_URL = import.meta.env.VITE_APP_BASE_API_URL;
 export const IDRIVE_URL = `https://u2e0.c18.e2-4.dev`;
 export const FFMPEG_URL = `https://unpkg.com/@ffmpeg/core-mt@0.12.4/dist/umd`;
 
