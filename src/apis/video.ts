3170639379e0a8235371ed7c713c0ab8862cf2e9
adultlee <seoungin1228@gmail.com>
2024-01-30 20:54:58 +0900

    feat: api 명세에 따른 api 모킹

--- src/apis/video.ts
+++ src/apis/video.ts
@@ -3,9 +3,12 @@ import { API } from '@constants/api';
 import {
   VideoAddReqDto,
   VideoItemResDto,
-  VideoListResDto,
+  MyVideoListResDto,
   VideoPreSignedResDto,
   VideoPublicToggleResDto,
+  OnlyRelatedVideoListResDto,
+  VideoRelatedInfoListResDto,
+  AllVideoListResDto,
 } from '@/types/video';
 
 export const postVideo = async (body: VideoAddReqDto) => {
@@ -23,10 +26,68 @@ export const postPreSignedUrl = async () => {
   });
 };
 
-export const getVideoList = async () => {
-  return await getAPIResponseData<VideoListResDto>({
+/**
+ * GET video/all
+ * 마이페이지에서 비디오 전체 리스트를 조회
+ * Res
+ * 'id' | 'thumbnail' | 'videoName' | 'videoLength' | 'visibility' | 'createdAt'
+ */
+export const getMyVideoList = async () => {
+  return await getAPIResponseData<MyVideoListResDto>({
     method: 'get',
     url: API.VIDEO_ALL,
+  });
+};
+
+/**
+ * GET video/public
+ * 공유페이지에서 서비스 전체 비디오 리스트를 조회
+ * Res
+ * 'id' | 'thumbnail' | 'videoName' | 'videoLength' | 'visibility' | 'createdAt'
+ */
+export const getAllVideoList = async () => {
+  return await getAPIResponseData<AllVideoListResDto>({
+    method: 'get',
+    url: API.VIDEO_PUBLIC,
+  });
+};
+
+/**
+ * GET video/all
+ * 모든 영상을 보여주는 페이지에서 서비스 전체 비디오 리스트를 조회
+ * Res
+ * 'id' | 'thumbnail' | 'videoName' | 'videoLength' | 'visibility' | 'createdAt'
+ */
+export const getALLVideoList = async () => {
+  return await getAPIResponseData<MyVideoListResDto>({
+    method: 'get',
+    url: API.VIDEO_ALL,
+  });
+};
+
+/**
+ * GET video/related/${videoId}
+ * 비디오 아이디로 연결된 비디오 리스트"만" 조회
+ * Res => Array
+ * 'id' | 'nickname' | 'url' | 'hash' | 'videoName' | 'createdAt' | 'visibility'
+ */
+export const getOnlyRelatedVideoList = async (videoId: number) => {
+  return await getAPIResponseData<OnlyRelatedVideoListResDto>({
+    method: 'get',
+    url: API.VIDEO_ID_ONLY_RELATED(videoId),
+  });
+};
+
+/**
+ * GET video/relate/${videoId}
+ * 비디오 아이디로 비디오와 연결되거나 안된 모든 비디오를 조회
+ * Res => Array
+ *'id' | 'isRelated' | 'visibility' | 'videoName' | 'createdAt'
+ */
+export const getVideoRelatedInfoList = async (videoId: number) => {
+  return await getAPIResponseData<VideoRelatedInfoListResDto>({
+    method: 'get',
+    url: API.VIDEO_ID_RELATED_INFO(videoId),
   });
 };
 
@@ -37,6 +98,12 @@ export const getVideoByHash = async (hash: string) => {
   });
 };
 
+/**
+ * GET video/${videoId}
+ * 비디오 아이디로 비디오를 단건 조회했을 때 응답 객체 타입
+ * Res
+ *  'id' | 'nickname' | 'url' | 'hash' | 'videoName' | 'createdAt' | 'visibility'
+ */
 export const getVideoById = async (videoId: number) => {
   return await getAPIResponseData<VideoItemResDto>({
     method: 'get',
