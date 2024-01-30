77927af4ffd7d1abf267b3d94f343290876097bd
adultlee <seoungin1228@gmail.com>
2024-01-30 16:39:35 +0900

    feat: Rocord 페이지 기능연결

--- src/components/interviewSettingPage/index.ts
+++ src/components/interviewSettingPage/index.ts
@@ -4,4 +4,5 @@ export { default as RecordRadio } from '@components/interviewSettingPage/RecordP
 export { InterviewSettingFooter } from '@components/interviewSettingPage/interviewSettingFooter';
 export { default as TermPageServiceTour } from './ServiceTour/TermPageServiceTour';
 export { default as QuestionSelectPageServiceTour } from './ServiceTour/QuestionSelectPageServiceTour';
+export { default as RecordPageServiceTour } from './ServiceTour/RecordPageServiceTour';
 export { default as VideoSettingPageServiceTour } from './ServiceTour/VideoSettingPageServiceTour';
