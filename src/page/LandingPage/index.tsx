54ef438d8a200a582bf3d9f75b781c8ddf6872f9
adultlee <seoungin1228@gmail.com>
2024-01-30 16:33:59 +0900

    feat: Landing 페이지에서의 ServiceTour를 연결

--- src/page/LandingPage/index.tsx
+++ src/page/LandingPage/index.tsx
@@ -1,28 +1,34 @@
-import { StartButton } from '@common/index';
+import { ServiceTourStep, StartButton } from '@common/index';
 import {
   GoogleLoginButton,
   LandingImage,
   LandingPageLayout,
   WelcomeBlurb,
 } from '@components/landingPage';
+import LandingPageServiceTour from '@components/landingPage/LadingPageServiceTour';
 import { css } from '@emotion/react';
 
 const LandingPage: React.FC = () => {
   return (
-    <LandingPageLayout>
-      <WelcomeBlurb />
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          row-gap: 2rem;
-        `}
-      >
-        <StartButton />
-        <GoogleLoginButton />
-      </div>
-      <LandingImage />
-    </LandingPageLayout>
+    <>
+      <LandingPageLayout>
+        <WelcomeBlurb />
+        <div
+          css={css`
+            display: flex;
+            flex-direction: column;
+            row-gap: 2rem;
+          `}
+        >
+          <ServiceTourStep stepIndex={1}>
+            <StartButton />
+          </ServiceTourStep>
+          <GoogleLoginButton />
+        </div>
+        <LandingImage />
+      </LandingPageLayout>
+      <LandingPageServiceTour />
+    </>
   );
 };
 
