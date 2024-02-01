2f2d43f0f0abd4867be7309d80bf6f9130d35390
adultlee <seoungin1228@gmail.com>
2024-02-02 02:27:00 +0900

    fix: 렌더링 과정중 예외처리

--- src/page/InterviewVideoListPage/index.tsx
+++ src/page/InterviewVideoListPage/index.tsx
@@ -10,34 +10,36 @@ import { theme } from '@styles/theme';
 const InterviewVideoListPage: React.FC = () => {
   const { data: allVideoList } = usePublicVideoListQuery();
 
-  return (
-    <InterviewVideoListPageLayout>
-      <Typography
-        variant="title1"
-        css={css`
-          margin: 0 0.5rem;
-          align-self: flex-start;
-        `}
-      >
-        면접 영상 목록
-      </Typography>
-      <Box
-        css={css`
-          display: grid;
-          grid-template-columns: repeat(2, 1fr);
-          justify-content: center;
-          gap: 1.5rem;
-          padding: 1.5rem;
-          @media (max-width: ${theme.breakpoints.tablet}) {
-            grid-template-columns: 1fr;
-            padding: 1rem;
-          }
-        `}
-      >
-        <VideoList videoList={allVideoList} />
-      </Box>
-    </InterviewVideoListPageLayout>
-  );
+  if (!allVideoList) return <></>;
+  else
+    return (
+      <InterviewVideoListPageLayout>
+        <Typography
+          variant="title1"
+          css={css`
+            margin: 0 0.5rem;
+            align-self: flex-start;
+          `}
+        >
+          면접 영상 목록
+        </Typography>
+        <Box
+          css={css`
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            justify-content: center;
+            gap: 1.5rem;
+            padding: 1.5rem;
+            @media (max-width: ${theme.breakpoints.tablet}) {
+              grid-template-columns: 1fr;
+              padding: 1rem;
+            }
+          `}
+        >
+          <VideoList videoList={allVideoList} />
+        </Box>
+      </InterviewVideoListPageLayout>
+    );
 };
 
 export default InterviewVideoListPage;
--- src/page/interviewVideoPage/index.tsx
+++ src/page/interviewVideoPage/index.tsx
@@ -13,70 +13,62 @@ import useOnlyRelatedVideoQuery from '@hooks/apis/queries/useOnlyRelatedVideoLis
 import { Box } from '@foundation/index';
 import { css } from '@emotion/react';
 import { theme } from '@styles/theme';
-import { IconButton } from '@common/VideoPlayer';
-// import { VideoShareModal } from '@components/interviewVideoPage/ShareRangeModal';
 
 const InterviewVideoPage: React.FC = () => {
   const { videoId } = useParams();
   const [errorInfo, setErrorInfo] = useState<Partial<Response>>();
   const { data: relatedVideoItem } = useOnlyRelatedVideoQuery(Number(videoId));
-  const [isOpen, setIsOpen] = useState(false);
-
   if (!videoId) return <Navigate to={PATH.ROOT} />;
-
-  return (
-    <InterviewVideoPageLayout>
-      <ErrorBoundary
-        onError={(err) => {
-          if (!isAxiosError(err)) return;
-          setErrorInfo({
-            status: err?.response?.status,
-            statusText: err.response?.statusText,
-          });
-        }}
-        fallback={<Navigate to={PATH.NOT_FOUND} state={errorInfo} />}
-      >
-        <Suspense
-          fallback={
-            <CenterLayout>
-              <LoadingBounce />
-            </CenterLayout>
-          }
+  if (!relatedVideoItem) return <></>;
+  else
+    return (
+      <InterviewVideoPageLayout>
+        <ErrorBoundary
+          onError={(err) => {
+            if (!isAxiosError(err)) return;
+            setErrorInfo({
+              status: err?.response?.status,
+              statusText: err.response?.statusText,
+            });
+          }}
+          fallback={<Navigate to={PATH.NOT_FOUND} state={errorInfo} />}
         >
-          <div
-            css={css`
-              display: flex;
-              flex-direction: column;
-              row-gap: 0.5rem;
-            `}
+          <Suspense
+            fallback={
+              <CenterLayout>
+                <LoadingBounce />
+              </CenterLayout>
+            }
           >
-            <IconButton // 해당 컴포넌트는 "나의" 영상임을 확인했을때만 반환되어야 합니다.
-              text="영상 수정하기"
-              iconName="edit"
-              onClick={() => setIsOpen(!isOpen)}
-            />
-            <DefaultVideoPlayer videoId={videoId} />
-            <Box
+            <div
               css={css`
-                display: grid;
-                grid-template-columns: repeat(2, 1fr);
-                justify-content: center;
-                max-width: 64svw;
-                gap: 1.5rem;
-                padding: 1.5rem;
-                @media (max-width: ${theme.breakpoints.tablet}) {
-                  grid-template-columns: 1fr;
-                  padding: 1rem;
-                }
+                display: flex;
+                flex-direction: column;
+                row-gap: 0.5rem;
               `}
             >
-              <VideoList videoList={relatedVideoItem} />
-            </Box>
-          </div>
-        </Suspense>
-      </ErrorBoundary>
-    </InterviewVideoPageLayout>
-  );
+              <DefaultVideoPlayer videoId={videoId} />
+              <Box
+                css={css`
+                  display: grid;
+                  grid-template-columns: repeat(2, 1fr);
+                  justify-content: center;
+                  max-width: 64svw;
+                  gap: 1.5rem;
+                  padding: 1.5rem;
+                  @media (max-width: ${theme.breakpoints.tablet}) {
+                    grid-template-columns: 1fr;
+                    padding: 1rem;
+                  }
+                `}
+              >
+                <VideoList videoList={relatedVideoItem} />
+              </Box>
+            </div>
+          </Suspense>
+        </ErrorBoundary>
+      </InterviewVideoPageLayout>
+    );
 };
 
 export default InterviewVideoPage;
