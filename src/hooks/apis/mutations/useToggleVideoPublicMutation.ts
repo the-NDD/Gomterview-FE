cc06f47dee31da6656a69bd17235387eca5273e1
adultlee <seoungin1228@gmail.com>
2024-02-02 02:26:35 +0900

    feat: patch api 개발

--- src/hooks/apis/mutations/useToggleVideoPublicMutation.ts
+++ src/hooks/apis/mutations/useToggleVideoPublicMutation.ts
@@ -6,14 +6,31 @@ import { patchVideoPublic } from '@/apis/video';
  * PATCH /video/${videoId}
  *
  * 비디오 공개, 비공개를 토글하기 위한 api입니다.
+ *
  */
+
 const useToggleVideoPublicMutation = (videoId: number) => {
   const queryClient = useQueryClient();
+
   return useMutation({
-    mutationFn: () => patchVideoPublic(videoId),
+    mutationFn: ({
+      videoName,
+      visibility,
+      relatedVideoIds,
+    }: {
+      videoName: string;
+      visibility: 'PUBLIC' | 'LINK_ONLY' | 'PRIVATE';
+      relatedVideoIds: number[];
+    }) => patchVideoPublic(videoId, videoName, visibility, relatedVideoIds),
     onSuccess: () => {
       void queryClient.invalidateQueries({
         queryKey: QUERY_KEY.VIDEO_ID(videoId),
+      });
+      void queryClient.invalidateQueries({
+        queryKey: QUERY_KEY.VIDEO_ID_ONLY_RELATED(videoId),
+      });
+      void queryClient.invalidateQueries({
+        queryKey: QUERY_KEY.VIDEO_ID_RELATED_INFO(videoId),
       });
     },
   });
