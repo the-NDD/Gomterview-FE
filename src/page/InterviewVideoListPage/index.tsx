50d7d0b6db5d7f9b5a70b443ec048b8ef8a05ecb
adultlee <seoungin1228@gmail.com>
2024-02-02 16:14:55 +0900

    feat: Suspense를 적용하기 전 로딩 컴포넌트 적용

--- src/page/InterviewVideoListPage/index.tsx
+++ src/page/InterviewVideoListPage/index.tsx
@@ -1,5 +1,6 @@
-import { VideoList } from '@common/index';
+import { LoadingBounce, VideoList } from '@common/index';
 import { InterviewVideoListPageLayout } from '@components/InterviewVideoListPage';
+import { CenterLayout } from '@components/layout';
 
 import { css } from '@emotion/react';
 
@@ -10,36 +11,41 @@ import { theme } from '@styles/theme';
 const InterviewVideoListPage: React.FC = () => {
   const { data: allVideoList } = usePublicVideoListQuery();
 
-  if (!allVideoList) return <></>;
-  else
-    return (
-      <InterviewVideoListPageLayout>
-        <Typography
-          variant="title1"
-          css={css`
-            margin: 0 0.5rem;
-            align-self: flex-start;
-          `}
-        >
-          면접 영상 목록
-        </Typography>
-        <Box
-          css={css`
-            display: grid;
-            grid-template-columns: repeat(2, 1fr);
-            justify-content: center;
-            gap: 1.5rem;
-            padding: 1.5rem;
-            @media (max-width: ${theme.breakpoints.tablet}) {
-              grid-template-columns: 1fr;
-              padding: 1rem;
-            }
-          `}
-        >
+  return (
+    <InterviewVideoListPageLayout>
+      <Typography
+        variant="title1"
+        css={css`
+          margin: 0 0.5rem;
+          align-self: flex-start;
+        `}
+      >
+        면접 영상 목록
+      </Typography>
+      <Box
+        css={css`
+          display: grid;
+          grid-template-columns: ${allVideoList ? 'repeat(2, 1fr)' : 'none'};
+          justify-content: center;
+          gap: 1.5rem;
+          padding: 1.5rem;
+          min-height: 18.75rem;
+          @media (max-width: ${theme.breakpoints.tablet}) {
+            grid-template-columns: 1fr;
+            padding: 1rem;
+          }
+        `}
+      >
+        {allVideoList ? (
           <VideoList videoList={allVideoList} />
-        </Box>
-      </InterviewVideoListPageLayout>
-    );
+        ) : (
+          <CenterLayout>
+            <LoadingBounce />
+          </CenterLayout>
+        )}
+      </Box>
+    </InterviewVideoListPageLayout>
+  );
 };
 
 export default InterviewVideoListPage;
