35fe613bc262209d51fbf4d74d825e9815b681e7
Sumin <robolindasoo@gmail.com>
2024-03-09 12:29:06 +0900

    test: ffmpeg 테스트

--- src/routes/landingLoader.ts
+++ src/routes/landingLoader.ts
@@ -1,13 +1,11 @@
-import { canUseFFmpeg, getFFmpeg } from '@/utils/record';
-import { json } from 'react-router-dom';
-
 const landingLoader = async () => {
-  await getFFmpeg(); // Root에서 FFmpeg module을 받습니다.
-  const isEncodingAllow = await canUseFFmpeg();
-
-  return json({
-    isEncodingAllow: isEncodingAllow,
-  });
+  // await getFFmpeg(); // Root에서 FFmpeg module을 받습니다.
+  // const isEncodingAllow = await canUseFFmpeg();
+  //
+  // return json({
+  //   isEncodingAllow: isEncodingAllow,
+  // });
+  return null;
 };
 
 export default landingLoader;
