546f1c141186016250054277332a405bc3230c02
Sumin <robolindasoo@gmail.com>
2024-03-09 20:31:01 +0900

    chore: dts 설정중

--- packages/design-system/vite.config.ts
+++ packages/design-system/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,12 +10,20 @@ export default defineConfig({
       entry: './src/index.ts',
       name: 'design-system',
       fileName: 'index',
-      formats: ['es'],
     },
     rollupOptions: {
       input: './src/index.ts',
       external: ['react', 'react-dom'],
     },
   },
-  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    preserveSymlinks: true,
+  },
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+    }),
+    tsconfigPaths(),
+  ],
 });
