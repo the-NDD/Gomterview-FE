d1b997a639050376e838fd80f02676bb5943870e
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-04-26 11:53:29 +0900

    chore: 번들링 설정 추가
    - ejs esm 지원
    - 타입도 하나의 파일로 합침

---
+++ packages/useModal/rollup.config.js
@@ -0,0 +1,35 @@
+const babel = require('@rollup/plugin-babel');
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const dts = require('rollup-plugin-dts').default;
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: './dist/bundle.js',
+        format: 'cjs',
+      },
+      {
+        file: './dist/bundle.mjs',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      nodeResolve({ extensions }),
+      commonjs(),
+      babel({ extensions, babelHelpers: 'bundled' }),
+    ],
+  },
+  {
+    input: 'types/index.d.ts',
+    output: {
+      file: './dist/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];
