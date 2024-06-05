eb64b3ebbcc637db3e3763ab8db07bdb29c5c28b
HaeMin Yoon <yunhatmi@gachon.ac.kr>
2024-06-05 14:43:39 +0900

    test: 테스트 타이밍을 요소가 나타날때 까지 기다린 후 검증하도록 변경

--- apps/gomterview-com/tests/landingPage.test.ts
+++ apps/gomterview-com/tests/landingPage.test.ts
@@ -9,12 +9,17 @@ test.describe('비회원 상태', () => {
   test('로그인 버튼과 비회원으로 시작하기 버튼이 보여야 한다', async ({
     page,
   }) => {
-    expect(
-      await page.locator('button:has-text("비회원으로 시작하기")').isVisible()
-    ).toBe(true);
-    expect(
-      await page.locator('button:has-text("Google로 시작하기")').isVisible()
-    ).toBe(true);
+    const NonMemberStartButton = page.locator(
+      'button:has-text("비회원으로 시작하기")'
+    );
+    await NonMemberStartButton.waitFor();
+    expect(await NonMemberStartButton.isVisible()).toBe(true);
+
+    const GoogleStartButton = page.locator(
+      'button:has-text("Google로 시작하기")'
+    );
+    await GoogleStartButton.waitFor();
+    expect(await GoogleStartButton.isVisible()).toBe(true);
   });
 
   test('튜토리얼이 스킵 되었으면 듀토리얼 시작하기 버튼이 보이도록 해야한다.', async ({
@@ -32,36 +37,35 @@ test.describe('비회원 상태', () => {
   });
 
   test('메인 페이지 듀토리얼이 동작한다.', async ({ page }) => {
-    await page.goto('/');
-
-    expect(
-      await page
-        .locator('#react-joyride-step-0')
-        .getByRole('alertdialog')
-        .isVisible()
-    ).toBe(true);
-
-    await page
+    const ServiceTourStartDialog = page
       .locator('#react-joyride-step-0')
-      .getByRole('alertdialog')
-      .getByRole('button', { name: 'Next' })
-      .click();
+      .getByRole('alertdialog');
+    await ServiceTourStartDialog.waitFor();
 
-    expect(
-      await page
-        .locator('#react-joyride-step-1')
-        .getByRole('alertdialog')
-        .isVisible()
-    ).toBe(true);
+    expect(await ServiceTourStartDialog.isVisible()).toBe(true);
+    await ServiceTourStartDialog.getByRole('button', { name: 'Next' }).click();
+
+    const ServiceTourStep1 = page
+      .locator('#react-joyride-step-1')
+      .getByRole('alertdialog');
+
+    await ServiceTourStep1.waitFor();
+    expect(await ServiceTourStep1.isVisible()).toBe(true);
 
     //TODO: overlay에 가려진 요소는 hover후 click을 진행해야 동작함
-    await page.locator('#virtual-step-target-1').getByRole('link').hover({
+    const ServiceTourTarget1 = page
+      .locator('#virtual-step-target-1')
+      .getByRole('link');
+    await ServiceTourTarget1.hover({
       force: true,
     });
-    await page.locator('#virtual-step-target-1').getByRole('link').click({
+    await ServiceTourTarget1.click({
       force: true,
     });
 
+    await page.waitForURL(
+      `${PATH.INTERVIEW_SETTING}?page=${SETTING_PATH.TERMS}`
+    );
     expect(page.url()).toContain(
       `${PATH.INTERVIEW_SETTING}?page=${SETTING_PATH.TERMS}`
     );
@@ -73,18 +77,25 @@ test.describe('회원 상태', () => {
   test("로그인 상태에는 'Google로 시작하기' 버튼이 보이지 않고 면접 연습 시작하기 버튼이 보여야 한다.", async ({
     page,
   }) => {
-    expect(
-      await page.locator('button:has-text("Google로 시작하기")').isVisible()
-    ).toBe(false);
-    expect(
-      await page.locator('button:has-text("면접 연습 시작하기")').isVisible()
-    ).toBe(true);
+    const GoogleStartButton = page.locator(
+      'button:has-text("Google로 시작하기")'
+    );
+    expect(await GoogleStartButton.isVisible()).toBe(false);
+
+    const InterviewStartButton = page.locator(
+      'button:has-text("면접 연습 시작하기")'
+    );
+    await InterviewStartButton.waitFor();
+    expect(await InterviewStartButton.isVisible()).toBe(true);
   });
+
   test('듀토리얼이 스킵 되었으면 듀토리얼 시작하기 버튼이 보이도록 해야한다.', async ({
     page,
   }) => {
-    expect(
-      await page.locator('button:has-text("튜토리얼 시작하기")').isVisible()
-    ).toBe(true);
+    const TutorialStartButton = page.locator(
+      'button:has-text("튜토리얼 시작하기")'
+    );
+    await TutorialStartButton.waitFor();
+    expect(await TutorialStartButton.isVisible()).toBe(true);
   });
 });
